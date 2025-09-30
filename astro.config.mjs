14ed7706260c9da73eee9a92f67f620e676668db
Johan Edlund <me@johanedlund.se>
2025-09-30 16:36:07 +0200

    astrojs-service-worker added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import serviceWorker from "astrojs-service-worker";
 
 import db from '@astrojs/db';
 
 // https://astro.build/config
 export default defineConfig({
   //output: 'server', // Required change. Pick either hybrid or server.output: 'server',
-  integrations: [db()]
+  integrations: [db(), serviceWorker()]
 });
